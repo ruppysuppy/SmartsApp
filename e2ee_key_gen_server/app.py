6386272859c4ae03cebafd63fb6e857b44bf389e
Tapajyoti Bose <ruppysuppy@gmail.com>
2020-11-30 12:25:04 +0530

    feat(end-to-end-encryption): added server for key exchange

---
+++ e2ee_key_gen_server/app.py
@@ -0,0 +1,32 @@
+from flask import Flask, jsonify, request
+
+from dh import DiffieHellman
+
+app = Flask(__name__)
+
+
+@app.route("/generate-keys", methods=["GET"])
+def generate_keys():
+    dh = DiffieHellman()
+    private_key, public_key = dh.get_private_key(), dh.gen_public_key()
+    return jsonify({"private_key": private_key, "public_key": public_key,})
+
+
+@app.route("/generate-shared-key", methods=["GET"])
+def generate_shared_key():
+    try:
+        local_private_key = request.args.get("local_private_key")
+        remote_public_key = request.args.get("remote_public_key")
+    except:
+        return jsonify({"message": "Invalid parameters"}), 400
+    try:
+        shared_key = DiffieHellman.gen_shared_key_static(
+            local_private_key, remote_public_key
+        )
+    except:
+        return jsonify({"message": "Invalid public key"}), 400
+    return jsonify({"shared_key": shared_key})
+
+
+if __name__ == "__main__":
+    app.run()
