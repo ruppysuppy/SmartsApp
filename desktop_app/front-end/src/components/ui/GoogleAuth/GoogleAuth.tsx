44e753260fbb95e97d93a84fd1cdff76c718b321
Tapajyoti Bose <ruppysuppy@gmail.com>
2020-12-22 11:14:48 +0530

    fix(desktop-app): restricted google oauth

--- desktop_app/front-end/src/components/ui/GoogleAuth/GoogleAuth.tsx
+++ desktop_app/front-end/src/components/ui/GoogleAuth/GoogleAuth.tsx
@@ -1,33 +1,12 @@
 import React from "react";
-import { connect } from "react-redux";
-
-import firebase, { auth } from "../../../firebase/firebase";
-import * as actions from "../../../store/actions/actions";
 
 import styles from "./googleAuth.module.css";
 
-interface IProps {
-	authChangedHandler: (user?: firebase.User) => void;
-}
-
-function GoogleAuth({ authChangedHandler }: IProps) {
-	const OAuthHalder = async () => {
-		const provider = new firebase.auth.GoogleAuthProvider();
-		auth.signInWithRedirect(provider);
-		const result = await auth.getRedirectResult();
-		const user = result.user;
-		if (user) {
-			authChangedHandler(user);
-		}
-	};
-
+function GoogleAuth() {
 	return (
 		<div className={styles.Body}>
 			<h5 className="text">OR</h5>
-			<div
-				className={`text ${styles.OAuthContainer}`}
-				onClick={OAuthHalder}
-			>
+			<div className={`text ${styles.OAuthContainer}`}>
 				<i className="fa fa-google" aria-hidden="true" />
 				Sign In with Google
 			</div>
@@ -35,9 +14,4 @@ function GoogleAuth({ authChangedHandler }: IProps) {
 	);
 }
 
-const mapDispatchToProps = (dispatch: any) => ({
-	authChangedHandler: (user?: firebase.User) =>
-		dispatch(actions.authChangedHandler(user)),
-});
-
-export default connect(null, mapDispatchToProps)(GoogleAuth);
+export default GoogleAuth;
